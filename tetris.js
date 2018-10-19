fb03ccd2e4ca298121318e9a55f88563f7467e0f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-10-19 09:40:42 -0400

    added merge and collide functions

--- tetris.js
+++ tetris.js
@@ -11,6 +11,30 @@ const matrix = [
   [0,1,0],
 ];
 
+function collide(arena,player) {
+  const [m,o] = [player.matrix,player.pos];
+  for(let y = 0; y < m.length; ++y) {
+    for(let x = 0; x < m[y].length; ++x) {
+      if(m[y][x] !== 0 &&
+          (arena[y + o.y] &&
+          arena[y + o.y][x + o.x]) !== 0) {
+            return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w,h) {
+  const matrix = [];
+  while(h--) {
+    matrix.push(new Array(w).fill(0))
+  }
+  return matrix;
+}
+
+
+
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0,0,canvas.width,canvas.height)
@@ -31,6 +55,26 @@ function drawMatrix(matrix,offset) {
   })
 }
 
+function merge(arena,player) {
+  player.matrix.forEach((row,y) => {
+    row.forEach((value,x) => {
+      if(value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  if(collide(arena,player)) {
+    player.pos.y--;
+    merge(arena,player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 let lastTime = 0;
@@ -40,12 +84,14 @@ function update(time = 0){
 
   dropCounter += deltaTime;
   if(dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
   draw();
   requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12,20);
+
 
 const player = {
   pos: {x: 5, y: 5},
